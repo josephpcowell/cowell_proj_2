e5bb4129810f8a05a82d55e5a2ef1bd37b7d79f7
Joe Cowell <37605645+josephpcowell@users.noreply.github.com>
2020-10-05 08:32:58 -0400

    MVP in progress
    Pulled 8000 movies and started shaping the data a little better

--- imdb_big_scrape.py
+++ imdb_big_scrape.py
@@ -5,23 +5,20 @@ from IPython.core.display import display, HTML
 import requests
 
 
-def get_movie_links(pages):
+def get_movie_links(pages, start=1):
     """
-    The basis is movies released between 2018 and 2020
-    input: number of pages you want to scrape
+    The basis is movies released between 2015 and 2020
+    input: number of pages you want to scrape and the starting page
     output: return list containing links to movie pages
     """
     links = []
-    num_pages = [1, 101, 201, 301, 401, 501, 601, 701, 801, 901]
     for i in range(pages):
-        start = num_pages[pages-1]
-        url = f'https://www.imdb.com/search/title/?title_type=feature&release_date=2018-01-01,2020-09-01&countries=us&languages=en&view=simple&count=100&start={start}&ref_=adv_nxt'
-        # url = f'https://www.imdb.com/search/title/?title_type=feature&release_date=2018-01-01,2020-09-01&view=simple&count=100&start={start}&ref_=adv_nxt'
-        # url = f'https://www.imdb.com/search/title/?groups=top_1000&view=simple&sort=user_rating,desc&count=100&start={start}&ref_=adv_nxt'
+        url = f"https://www.imdb.com/search/title/?title_type=feature&release_date=2015-01-01,2020-05-01&countries=us&languages=en&view=simple&count=100&start={start}&ref_=adv_nxt"
         response = requests.get(url)
         page = response.text
         soup = BeautifulSoup(page)
-        search1 = soup.find_all(class_='lister-item-index unbold text-primary')
-        link_list = [i.findNext().findChildren()[0]['href'] for i in search1]
+        search1 = soup.find_all(class_="lister-item-index unbold text-primary")
+        link_list = [i.findNext().findChildren()[0]["href"] for i in search1]
         links.extend(link_list)
+        start += 100
     return links
